94381449223660eb04b252b855c5c6c5ac7c9a47
arainay <arainay@gmail.com>
2019-02-06 20:12:20 +0800

    dependencies are updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,14 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, lazy } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Navigation from '../Navigation';
-import Profile from '../Profile';
-import Organization from '../Organization';
+// import Profile from '../Profile';
+// import Organization from '../Organization';
 
 import * as ROUTES from '../../constants/routes';
+
+const Organization = lazy(() => import('../Organization'));
+const Profile = lazy(() => import('../Profile'));
 
 const App = () => {
   return (
